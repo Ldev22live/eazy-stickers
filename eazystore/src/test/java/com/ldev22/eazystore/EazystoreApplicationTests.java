eb0acb750d6bdfcddf2e72020b73beafe3667496
ldev22 <ldev22@live.com>
2026-06-12 12:57:36 +0200

    Initial

---
+++ eazystore/src/test/java/com/ldev22/eazystore/EazystoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldev22.eazystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazystoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
